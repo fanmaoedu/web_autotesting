0bbffdaf0b377844df53149c60b02c8c73daf3c6
zhangsan <17081371097@163.com>
2019-01-03 23:16:41 +0800

    init lesson 01

---
+++ lesson-01/start/auto_jd.py
@@ -0,0 +1,34 @@
+
+import unittest
+from selenium import webdriver
+
+class WeiboLogin(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome()
+
+    
+    def setUp(self):
+        driver = self.driver
+        driver.maximize_window()
+
+
+
+    def test_login(self):
+        pass
+
+
+
+    def tearDown(self):
+        driver = self.driver
+        driver.delete_all_cookies()
+        driver.save_screenshot('./weibo.png')
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+
+if __name__ == "__main__":
+    unittest.main()
+    
